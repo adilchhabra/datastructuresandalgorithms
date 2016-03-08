226465b3b5bc3e0d0f8be34654e3e8acc67b0568
adilchhabra <adilchhabra7@gmail.com>
2016-03-07 22:09:56 -0500

    Delete randomGen.java

--- randomGen.java
+++
@@ -1,9 +0,0 @@
-public class randomGen {
-	public static void main(String[] args) {
-		int [] a = {1,2,3,4,5,6,7,8,9,10};
-		int p = 0;
-		int r = a.length;
-		int ran = (int) (Math.random()*(r-p));
-		System.out.println(ran);
-	}
-}
